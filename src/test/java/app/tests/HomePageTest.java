d93d14a3c3307785eea83428e0f3416f7d6db4d8
SS <subha9696@gmail.com>
2020-01-12 00:56:39 -0800

    creating

--- src/test/java/app/tests/HomePageTest.java
+++ src/test/java/app/tests/HomePageTest.java
@@ -17,7 +17,7 @@ public class HomePageTest {
 		
 	}
 	@Test
-	public void verifyWomenTags() {
+	public void verifyWomenTags12() {
 		Assert.assertTrue(bp.elementFound(hp.getWomenTag()),"Failed to display");
 		
 	}
